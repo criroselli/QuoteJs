41cc1ff0b0a3c689050cb4f751e374c26060840b
criroselli <cri.roselli@gmail.com>
2014-01-16 17:00:38 +0100

    Update citazioni.js

--- citazioni.js
+++ citazioni.js
@@ -2,6 +2,13 @@ return [
   /* { 
        citazione:"", 
        data:"",
+       description:""
     }
   */  
+  { 
+    citazione:"Serve un anello per dominarli tutto",
+    description:"Rifertendosi ad un BUG",
+    data: "Thu Jan 16 2014 17:00:05 GMT+0100 (CET)"
+  },
+  
 ];
