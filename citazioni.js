8ce997ebe1851b5eed28a9862003e452b58fe205
criroselli <cri.roselli@gmail.com>
2014-01-21 18:00:21 +0100

    Update citazioni.js

--- citazioni.js
+++ citazioni.js
@@ -8,19 +8,24 @@ return [
   { 
     citazione:"Serve un anello per dominarli tutti",
     description:"Rifertendosi ad un BUG",
-    data: "Thu Jan 16 2014 17:00:05 GMT+0100 (CET)"
+    data: "Jan 16 2014 17:00:05 GMT+0100 (CET)"
   },{ 
     citazione:"Non voglio fare un deploy alle 17:00",
     description:"Rifertendosi ad un DEPLOY un po' tardivo",
-    data: "Thu Jan 16 2014 17:05:05 GMT+0100 (CET)"
+    data: "Jan 16 2014 17:05:05 GMT+0100 (CET)"
   },{ 
     citazione:"Meglio che non funziona mai",
     description:"Riferendosi al deploy del brick Mailchimp che non funzionava in produzione",
-    data: "Thu Jan 16 2014 18:25:35 GMT+0100 (CET)"
+    data: "Jan 16 2014 18:25:35 GMT+0100 (CET)"
   },
   {
     citazione:"Non era pronto per andare senza rotelle",
     description:"RIferendosi ad un email inviata a 1200 persone per l'editor",
-    data: "Thu Jan 17 2014 13:35:35 GMT+0100 (CET)"
+    data: "Jan 17 2014 13:35:35 GMT+0100 (CET)"
+  },
+   {
+    citazione:"Dobbiamo fa la fusion",
+    description:"Riferendosi all'unione di due file grunt",
+    data: "Jan 21 2014 17:59:35 GMT+0100 (CET)"
   }
 ];
