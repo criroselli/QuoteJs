606330d66e86ef3d084a06ccf5cf60626a2c50b7
criroselli <cri.roselli@gmail.com>
2014-01-23 14:38:45 +0100

    Update citazioni.js

--- citazioni.js
+++ citazioni.js
@@ -17,15 +17,17 @@ return [
     citazione:"Meglio che non funziona mai",
     description:"Riferendosi al deploy del brick Mailchimp che non funzionava in produzione",
     data: "Jan 16 2014 18:25:35 GMT+0100 (CET)"
-  },
-  {
+  },{
     citazione:"Non era pronto per andare senza rotelle",
     description:"RIferendosi ad un email inviata a 1200 persone per l'editor",
     data: "Jan 17 2014 13:35:35 GMT+0100 (CET)"
-  },
-   {
+  },{
     citazione:"Dobbiamo fa la fusion",
     description:"Riferendosi all'unione di due file grunt",
     data: "Jan 21 2014 17:59:35 GMT+0100 (CET)"
+  },{
+    citazione:"Questa é proprio una cazzata",
+    description:"Riferendosi all'ascensore sociale tirato in ballo dal Presidente Giuliano Iacobelli",
+    data: "Jan 23 2014 14:38:35 GMT+0100 (CET)"
   }
 ];
