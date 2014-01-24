d7514e82570886ae2372459aa34b992a50c4d6c8
criroselli <cri.roselli@gmail.com>
2014-01-24 18:21:08 +0100

    Update citazioniGiuliano.js

--- citazioniGiuliano.js
+++ citazioniGiuliano.js
@@ -1,2 +1,13 @@
-return {
-}
+return [
+  /* { 
+       citazione:"", 
+       data:"",
+       description:""
+    }
+  */  
+  { 
+    citazione:"Sono un ascensore sociale",
+    description:"Rifertendosi al poco tempo passato con Cristina",
+    data: "Jan 23 2014 12:30:05 GMT+0100 (CET)"
+  }
+];
