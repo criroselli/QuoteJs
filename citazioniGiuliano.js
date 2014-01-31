b3e5b2ee47fa8e5a45ff448a9d9ebf2520b2595a
criroselli <cri.roselli@gmail.com>
2014-01-31 13:59:53 +0100

    Update citazioniGiuliano.js

--- citazioniGiuliano.js
+++ citazioniGiuliano.js
@@ -9,5 +9,11 @@ return [
     citazione:"Sono un ascensore sociale",
     description:"Rifertendosi al poco tempo passato con Cristina",
     data: "Jan 23 2014 12:30:05 GMT+0100 (CET)"
-  }
+  },{ 
+    citazione:"Hai fatto un corso di educazione in curva Sud",
+    description:"Rifertendosi alla poca educazione del ragazzo che consegna i panini di Just Eat",
+    data: "Jan 31 2014 13:56:05 GMT+0100 (CET)"
+  },
+  
+  
 ];
