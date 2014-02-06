93036f9502223f1f6b5f40a6c873159bc007643c
criroselli <cri.roselli@gmail.com>
2014-02-06 14:10:50 +0100

    Update citazioniGiuliano.js

--- citazioniGiuliano.js
+++ citazioniGiuliano.js
@@ -13,7 +13,11 @@ return [
     citazione:"Hai fatto un corso di educazione in curva Sud",
     description:"Rifertendosi alla poca educazione del ragazzo che consegna i panini di Just Eat",
     data: "Jan 31 2014 13:56:05 GMT+0100 (CET)"
-  },
+  },{ 
+    citazione:"Arrivo",
+    description:"Riferendosi a Zzub",
+    data: "Feb 06 2014 11:45:05 GMT+0100 (CET)"
+  }
   
   
 ];
